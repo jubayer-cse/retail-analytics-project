448ab277dda54c2973eea12bc73894bd0fd12bc7
Jubayer <jubayer52786@gmail.com>
2026-07-18 19:01:06 +0600

    Update q3_revenue_by_country.sql

--- sql/q3_revenue_by_country.sql
+++ sql/q3_revenue_by_country.sql
@@ -1,1 +1,11 @@
+-- Query 3: Revenue by Country
+-- Shows revenue and customer count per country
+-- Answers: Where is revenue concentrated geographically?
 
+SELECT
+  "Country",
+  ROUND(SUM("TotalPrice")::numeric, 2) AS revenue,
+  COUNT(DISTINCT "CustomerID") AS customers
+FROM transactions
+GROUP BY "Country"
+ORDER BY revenue DESC;
